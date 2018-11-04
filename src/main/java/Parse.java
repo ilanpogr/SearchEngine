b9559b1961ceafa2073c672c27005aa6a41500b6
ilanpogr <ilanpogreb01@gmail.com>
2018-11-04 20:42:22 +0200

    Thousand parse done and working. NOT INCLUDING next word "Thousand".

---
+++ src/main/java/Parse.java
@@ -0,0 +1,59 @@
+import java.util.List;
+import java.util.ListIterator;
+
+public class Parse {
+
+    private static String[] cutDecimal(String str) {
+        String[] result = new String[2];
+        int index = str.indexOf('.');
+        String tmp = "";
+        if (index != -1) {
+            tmp = str.substring(index + 1, str.length());
+            str = str.substring(0, index);
+        }
+        result[0] = str;
+        result[1] = tmp;
+        return result;
+    }
+
+    /**
+     * Parameter String[] number: number[0] is Integer, number[1] is decimal.
+     *
+     * @param current: the current number we work with
+     * @return String after parsing.
+     */
+    private static String numberThousand(String current) {
+        current = current.replace(",", "");
+        String[] number = cutDecimal(current);
+        int integer = Integer.parseInt(number[0]);
+        if (integer >= 1000 && integer < 1000000) {
+            int decimal = integer % 1000;
+            String tmp = Integer.toString(decimal);
+            if (decimal < 10) {
+                tmp = "00" + tmp;
+            } else if (decimal < 100) {
+                tmp = '0' + tmp;
+            }
+            integer = integer / 1000;
+            number[0] = Integer.toString(integer);
+            number[1] = tmp + number[1] + 'K';
+            if (number[1].length() > 1) {
+                current = number[0] + '.' + number[1];
+            } else {
+                current = number[0] + number[1];
+            }
+        }
+        return current;
+    }
+
+
+    /*TODO -
+        1. if next token is 'Thousand' or 1000 < int < 1000000 --> numberThousand
+      */
+    public static void main(String[] args) {
+        String num = numberThousand("10003.24");
+        System.out.println(num);
+    }
+
+
+}
