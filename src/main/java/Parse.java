f4ab5bc75dfcd60321893776a32bc5a046f3af55
ilanpogr <ilanpogreb01@gmail.com>
2018-11-04 21:33:44 +0200

    Million parse done and working. NOT INCLUDING next word "Million".

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -3,6 +3,11 @@ import java.util.ListIterator;
 
 public class Parse {
 
+    /**
+     * Seperate the Integer from the Decimal number/
+     * @param str: the String that representing the whole number
+     * @return String array. [0] - Integer; [1] - Decimal.
+     */
     private static String[] cutDecimal(String str) {
         String[] result = new String[2];
         int index = str.indexOf('.');
@@ -17,6 +22,7 @@ public class Parse {
     }
 
     /**
+     * Parsing numbers between thousand and million
      * Parameter String[] number: number[0] is Integer, number[1] is decimal.
      *
      * @param current: the current number we work with
@@ -26,14 +32,9 @@ public class Parse {
         current = current.replace(",", "");
         String[] number = cutDecimal(current);
         int integer = Integer.parseInt(number[0]);
-        if (integer >= 1000 && integer < 1000000) {
+          if (integer >= 1000 && integer < 1000000) {
             int decimal = integer % 1000;
-            String tmp = Integer.toString(decimal);
-            if (decimal < 10) {
-                tmp = "00" + tmp;
-            } else if (decimal < 100) {
-                tmp = '0' + tmp;
-            }
+            String tmp = extremeCaseDecimal(decimal, 'K');
             integer = integer / 1000;
             number[0] = Integer.toString(integer);
             number[1] = tmp + number[1] + 'K';
@@ -46,12 +47,61 @@ public class Parse {
         return current;
     }
 
+    /**
+     * Parsing numbers between million and billion
+     * Parameter String[] number: number[0] is Integer, number[1] is decimal.
+     *
+     * @param current: the current number we work with
+     * @return String after parsing.
+     */
+    private static String numberMillion(String current) {
+        current = current.replace(",", "");
+        String[] number = cutDecimal(current);
+        int integer = Integer.parseInt(number[0]);
+        if (integer < 1000000000 && integer >= 1000000) {
+            int decimal = integer % 1000000;
+            String tmp = extremeCaseDecimal(decimal, 'M');
+            integer = integer / 1000000;
+            number[0] = Integer.toString(integer);
+            number[1] = tmp + number[1] + 'M';
+            if (number[1].length() > 1) {
+                current = number[0] + '.' + number[1];
+            } else {
+                current = number[0] + number[1];
+            }
+        }
+        return current;
+    }
+
+    private static String extremeCaseDecimal(int decimal, char type) {
+        String str = Integer.toString(decimal);
+        if(type == 'K'){
+            if (decimal < 10) {
+                str = "00" + str;
+            } else if (decimal < 100) {
+                str = '0' + str;
+            }
+        }else if (type == 'M'){
+            if (decimal < 10) {
+                str = "00000" + str;
+            } else if (decimal < 100) {
+                str = "0000" + str;
+            } else if (decimal < 1000) {
+                str = "000" + str;
+            } else if (decimal < 10000) {
+                str = "00" + str;
+            } else if (decimal < 100000) {
+                str = '0' + str;
+            }
+        }
+        return str;
+    }
 
     /*TODO -
         1. if next token is 'Thousand' or 1000 < int < 1000000 --> numberThousand
       */
     public static void main(String[] args) {
-        String num = numberThousand("10003.24");
+        String num = numberMillion("100,007,703.784");
         System.out.println(num);
     }
 
