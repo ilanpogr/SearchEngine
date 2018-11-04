ba11e9d2b00cec85704a978e271341a5378eb7a1
ilanpogr <ilanpogreb01@gmail.com>
2018-11-04 18:27:24 +0200

    ReadFile Class ready for now. starting Parsing Branch PAY ATTENTION: if ReadFile Class had changes.. MAKE SURE to do merge and compare correctly!

--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -4,11 +4,22 @@ import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
-import java.util.Properties;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ReadFile {
+
+    private static String regEx_Text = "^(<TEXT>[^<]*</TEXT>)$";
+
+
+
+//        private String getFromPropertiesBaseDir() throws IOException {
+//        Properties props = new Properties();
+//        props.load(this.getClass().getResourceAsStream("project.properties"));
+//        return (String)props.get("project.basedir");
+//    }
+
 
     /**
      * Apply Regular Expressions on the contents of a file
@@ -31,32 +42,36 @@ public class ReadFile {
         return cbuf;
     }
 
-//    private String getFromPropertiesBaseDir() throws IOException {
-//        Properties props = new Properties();
-//        props.load(this.getClass().getResourceAsStream("project.properties"));
-//        return (String)props.get("project.basedir");
-//    }
-
     //TODO: learn how to use maven: properties and on and on and on and on..
 
-    public static void main(String[] args) {
-        String baseDir = (String)System.getProperties().get("user.dir");
-        String filesPath =  baseDir + "/src/main/java/FB396001";
+    private static List<String> readByRegEx(String filesPath, String regEx){
+        List<String> textDic = new LinkedList<>();
         try {
             // Create matcher on file
-            //"^<TEXT>[\\w\\W]*</TEXT>$"
-            String regEx = "<TEXT>[^<]*</TEXT>";
-            Pattern pattern = Pattern.compile(regEx);
+            Pattern pattern = Pattern.compile(regEx,Pattern.MULTILINE);
             Matcher matcher = pattern.matcher(fromFile(filesPath));
             // Find all matches
             while (matcher.find()) {
                 if(matcher.group().length() != 0){
-                    System.out.println(matcher.group().trim());
+//                    System.out.println(matcher.group().trim());
+                    textDic.add(matcher.group().trim());
                 }
-                // Get the matching string
             }
         } catch (IOException e) {
             e.getStackTrace();
+        }
+        return textDic;
+    }
+
+
+    public static void main(String[] args) {
+        String baseDir = (String)System.getProperties().get("user.dir");
+        String filesPath =  baseDir + "/src/main/java/FB396001";
+        List<String> textDic = readByRegEx(filesPath, regEx_Text);
+        while(!textDic.isEmpty() && textDic.iterator().hasNext()){
+            System.out.println(textDic.iterator().next());
+            textDic.iterator().next();
+
         }
     }
 }
