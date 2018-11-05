a6e22372bb76b336749e81af5040ce3d89d688f9
Shawn <shawn@post.bgu.ac.il>
2018-11-05 17:12:17 +0200

    start of parse mehod. removeSuffix from token. fixing after code loss

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -1,9 +1,37 @@
-import java.util.List;
-import java.util.ListIterator;
+import javafx.util.Pair;
+
+import java.util.*;
 
 public class Parse {
 
     private static boolean nextUsed = false;
+    public static String testString = "just checking some of the rules, Ilan is a name so it sopposed to be capital letters November is a month and 11 NOV 2018 is a date. the number 1,203,453.22 should change and 99 percent that we will catch the numbers 1,243,535,238.3 without spending 2000 dollar. there are few more rules like NBA names and stuff.";
+    public static HashSet<Character> specialCharSet = initSpecialSet();
+    public static HashSet<String> monthSet = initMonthSet();
+
+    private static HashSet<String> initMonthSet() {
+        monthSet = new HashSet<>();
+        String[] months = {"JANUARY", "JAN", "FEBRUARY", "FEB", "MARCH", "MAR", "APRIL", "APR",
+                "MAY", "JUNE", "JUN", "JULY", "JUL", "AUGUST", "AUG", "SEPTEMBER", "SEP",
+                "OCTOBER", "OCT", "NOVEMBER", "NOV", "DECEMBER", "DEC"};
+
+        for (int i = 0; i < months.length; ++i) {
+            monthSet.add(months[i]);
+        }
+
+        return monthSet;
+    }
+
+    private static HashSet<Character> initSpecialSet() {
+        specialCharSet = new HashSet<>();
+        Character[] characters = new Character[]{'[', '(', '{', '`', ')', '<', '|', '&', '~', '+', '^', '@', '*', '?', '$', '.', '>', ';', '_', '\'', ':', ']', '/', '\\', '}', '!', '=', '#', ',', '"', '-'};
+
+        for (int i = 0; i < characters.length; ++i) {
+            specialCharSet.add(characters[i]);
+        }
+
+        return specialCharSet;
+    }
 
     /**
      * Seperate the Integer from the Decimal number/
@@ -187,8 +215,50 @@ public class Parse {
         3. if 'Trillion' next token - add 3 zeroes
         4/ if next token is 'Mathematical fracture' add to previous token!
       */
+
+    public static HashMap<String, String> parse(String[] str) {
+        HashMap<String, String> termsDict = new HashMap<>();
+        parseTokens(termsDict, str);
+        if (str[1].length() != 0) {
+            parseTokens(termsDict, str);
+        }
+
+        return termsDict;
+    }
+
+    private static void parseTokens(HashMap<String, String> termsDict, String[] str) {
+        String[] s = str[0].split(" ");
+        for (int i=0, lastIndex = s.length - 1; i <= lastIndex; ++i) {
+            String[] token = new String[]{s[i], "0,"};
+            removeSuffix(token);                //removes the suffix from the token (prefix too)
+            if (monthSet.contains(token[0].toUpperCase())){
+                String [] nextToken = {"","0,"};
+                if (i<lastIndex) {
+                    nextToken[0] = s[i+1];
+                    removeSuffix(nextToken);    //removes the suffix from the token (prefix too)
+                    nextToken[1]=nextToken[1].substring(0,nextToken[1].length()-1); //remove the stemmer bool
+                }
+            }
+
+
+        }
+
+    }
+
+    private static void removeSuffix(String[] token) {
+        boolean isSuffixRemoved = false;
+        String tok = token[0];
+        for (; tok.length() > 0 && specialCharSet.contains(tok.charAt(0)); tok = token[0] = tok.substring(1)) ;
+        while (tok.length() > 1 && specialCharSet.contains(tok.charAt(tok.length() - 1))) {
+            tok = token[0] = tok.substring(0, tok.length() - 1);
+            isSuffixRemoved = true;
+        }
+        token[1] += isSuffixRemoved ? "1" : "0";
+    }
+
+
     public static void main(String[] args) {
-        String str1 = "Let's make some strings and write numbers.. we should see only the numbers changing!";
+        /*String str1 = "Let's make some strings and write numbers.. we should see only the numbers changing!";
         String[] str2 = {"Kid", "with", "4312", "bananas", "wished", "to", "sell", "1341", "2/5", "of", "them", "to", "russian", "mafia"};
         String[] str4 = {"7", "3/2", "Thousand", "is", "a", "smaller", "number", "then", "40", "Trillion"};
         String[] str5 = {"1010.56", "is", "in", "the", "assignment", "pdf"};
@@ -205,6 +275,46 @@ public class Parse {
             for (int j = 0; j < strArray[i].length ; j++) {
 
             }
+        }*/
+        /*Character[] characters = new Character[]{'[', '(', '{', '`', ')', '<', '|', '&', '~', '+', '^', '@', '*', '?', '$', '.', '>', ';', '_', '\'', ':', ']', '/', '\\', '}', '!', '=', '#', ',', '"', '-'};
+        double start = (double) System.currentTimeMillis();
+
+        for (int i = 0; i < characters.length*2000; i++) {
+            if (speChar.contains(""+characters[i%characters.length]))
+                System.out.print(i+',');
         }
+
+        double end = (double) System.currentTimeMillis();
+        System.out.println();
+        System.out.println(end - start);
+        System.out.println();
+        start = (double) System.currentTimeMillis();
+
+        for (int i = 0; i < characters.length*2000; i++) {
+            if (specialCharSet.contains(characters[i%characters.length]))
+                System.out.print(i+',');
+        }
+
+        end = (double) System.currentTimeMillis();
+        System.out.println();
+        System.out.println(end - start);
+        System.out.println();
+
+        start = (double) System.currentTimeMillis();
+        speChar.contains("\\");
+        end = (double) System.currentTimeMillis();
+        System.out.println(end - start);
+        start = (double) System.currentTimeMillis();
+        specialCharSet.contains("\\");
+        end = (double) System.currentTimeMillis();
+        System.out.println(end - start);*/
+
+        String s1 = "7.032133";
+        Double d = Double.parseDouble(s1);
+        System.out.println(d);
+        System.out.println();
+        String[] s = new String[]{testString, ""};
+        parse(s);
+        System.out.println(s[0]);
     }
 }
