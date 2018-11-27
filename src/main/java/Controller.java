a130456129f9ddb07d691d0a8eac3d32982179db
Shawn <shawn@post.bgu.ac.il>
2018-11-27 19:59:56 +0200

    stemmer fixed case, not checked

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -26,27 +26,28 @@ public class Controller {
             String path = "C:\\Users\\User\\Documents\\לימודים\\אחזור מידע\\מנוע חיפוש\\corpus";
             filesList = new ArrayList<>();
             ReadFile readFile = new ReadFile(path);
+            Parse p = new Parse();
             double fileparse = 0;
             double singleparse = 0;
             while (readFile.hasNextFile()) {
                 f++;
                 double read = System.currentTimeMillis();
                 filesList = readFile.getFileList();
-                for (int i = 0; i < /*filesList.size()*/2; i++) {
+                for (int i = 0; i < filesList.size(); i++) {
                     double parsestart = System.currentTimeMillis();
                     currPath = filesList.get(i).docNum;
 //                    if (String.valueOf(filesList.get(i).text).isEmpty())
 //                        System.out.println("Current File: " + currPath + " (number " + f + ") in Doc number: " + ii);
-                    HashMap<String, String> map = new Parse().parse(new String[]{String.valueOf(filesList.get(i).text)});
+                    HashMap<String, String> map = p.parse(new String[]{String.valueOf(filesList.get(i).text)});
                     Stemmer stemmer =new Stemmer();
                     HashMap<String, String> stemmed = stemmer.stem(map);
-                    ArrayList<String> sm = new ArrayList<>(map.keySet());
-                    Collections.sort(sm);
-                    System.out.println(sm.toString());
-                    ArrayList<String> sst = new ArrayList<>(stemmed.keySet());
-                    Collections.sort(sst);
-                    System.out.println(sst.toString());
-
+//                    ArrayList<String> sm = new ArrayList<>(map.keySet());
+//                    Collections.sort(sm);
+//                    System.out.println(sm.toString());
+//                    ArrayList<String> sst = new ArrayList<>(stemmed.keySet());
+//                    Collections.sort(sst);
+//                    System.out.println(sst.toString());
+//
                     updateDocsMaxTf(filesList.get(i), map);
                     double parseend = System.currentTimeMillis();
                     singleparse = (parseend - read) / 1000;
@@ -54,7 +55,7 @@ public class Controller {
                     j++;
                     ii=i;
                 }
-            if (f % 100 == 0)
+            if (f % 15 == 0)
                 System.out.println("Time took to read and parse file: " + currPath + ": " + singleparse + " seconds. \t Total read and parse time: " + (int)fileparse + " seconds. \t (number of documents: " + (j) + ",\t number of files: " + f + ")");
                 filesList.clear();
             }
--- src/main/java/Stemmer/Stemmer.java
+++ src/main/java/Stemmer/Stemmer.java
@@ -2,79 +2,96 @@ package Stemmer;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.tartarus.snowball.SnowballStemmer;
 import org.tartarus.snowball.ext.englishStemmer;
 
-import static org.apache.commons.lang3.StringUtils.split;
-import static org.apache.commons.lang3.StringUtils.trim;
+import static org.apache.commons.lang3.StringUtils.*;
 //import
 
 public class Stemmer {
 
-    private static Map<String,String> cache = new HashMap<>();
-    private HashMap<String,String> stemmed;
+    private static Map<String, String> cache = new HashMap<>();
+    private HashMap<String, String> stemmed;
     private SnowballStemmer snowballStemmer = new englishStemmer();
 
-    public void clear(){
+    public void clear() {
         cache.clear();
         stemmed.clear();
     }
 
-    public HashMap<String,String> stem(HashMap<String,String> parsedDic){
+    public HashMap<String, String> stem(HashMap<String, String> parsedDic) {
         stemmed = new HashMap<>();
-        for (Map.Entry<String,String> term : parsedDic.entrySet()
-             ) {
-            boolean isUppercase = Character.isUpperCase(term.getKey().charAt(0));
-            //todo - to lower - stem - to upper case
-            if (term.getValue().endsWith("0")){
-                stemmed.put(term.getKey(),term.getValue());
+        for (Map.Entry<String, String> term : parsedDic.entrySet()
+        ) {
+            if (term.getValue().endsWith("0")) {
+                stemmed.put(term.getKey(), term.getValue());
                 continue;
-            } else{
-                if (cache.containsKey(term.getKey())){//if we stemmed it
-                    stemmed.put(cache.get(term.getKey()),term.getValue());//add it
+            }
+
+            //todo - to lower - stem - to upper case
+            boolean isUppercase = false;
+            StringBuilder stemmedTerm = new StringBuilder(), currentStemmed = new StringBuilder();
+            String s = term.getKey();
+            if (Character.isUpperCase(s.charAt(0))) {
+                isUppercase = true;
+                if (cache.containsKey(s)){
+                    if (cache.containsKey(lowerCase(s))){
+                        s= lowerCase(s);
+                        cache.replace(term.getKey(),cache.get(lowerCase(s)));
+                    }
+                    stemmed.put(cache.get(s),term.getValue());
                     continue;
                 }
-                StringBuilder stemmedTerm = new StringBuilder(),currentStemmed = new StringBuilder();;
-                if (term.getKey().contains(" ")){//if has more than 1 word
-                    String [] split = split(term.getKey()," ");
-                    for (String token:split
-                         ) {
-                        if (cache.containsKey(token)){
-                            currentStemmed.append(cache.get(token));
-                        }else{
-                            snowballStemmer.setCurrent(token);
-                            snowballStemmer.stem();
-                            currentStemmed.append(snowballStemmer.getCurrent());
-                            cache.put(token, currentStemmed.toString());
-                        }
-                        stemmedTerm.append(currentStemmed).append(" ");
-                        currentStemmed.setLength(0);
-                    }
-                    trim(stemmedTerm.toString());
-                } else {
-                    snowballStemmer.setCurrent(term.getKey());
-                    snowballStemmer.stem();
-                    stemmedTerm.append(snowballStemmer.getCurrent());
-                } if (stemmed.containsKey(stemmedTerm.toString())){
-                    currentStemmed.append(split(stemmed.get(stemmedTerm.toString()),",")[0]);
-                    sumFrequency(stemmed,stemmedTerm,currentStemmed,term.getValue());
-                } else {
-                    stemmed.put(stemmedTerm.toString(),term.getValue());
-                }
-                cache.put(term.getKey(),stemmedTerm.toString());
             }
+            if (cache.containsKey(s)){
+                stemmed.put(cache.get(s),term.getValue());
+                continue;
+            }
+            if (s.contains(" ")) {//if has more than 1 word
+                String[] split = split(s, " ");
+                for (String token : split
+                ) {
+                    if (cache.containsKey(token)) {
+                        currentStemmed.append(cache.get(token));
+                    } else {
+                        snowballStemmer.setCurrent(token);
+                        snowballStemmer.stem();
+                        currentStemmed.append(snowballStemmer.getCurrent());
+                        if (isUppercase){
+                            token = upperCase(token);
+                        }
+                        cache.put(token, currentStemmed.toString());
+                    }
+                    stemmedTerm.append(currentStemmed).append(" ");
+                    currentStemmed.setLength(0);
+                }
+                trim(stemmedTerm.toString());
+            } else {
+                snowballStemmer.setCurrent(s);
+                snowballStemmer.stem();
+                stemmedTerm.append(isUppercase?upperCase(snowballStemmer.getCurrent()):snowballStemmer.getCurrent());
+            }
+            if (stemmed.containsKey(stemmedTerm.toString())) {
+                currentStemmed.append(split(stemmed.get(stemmedTerm.toString()), ",")[0]);
+                sumFrequency(stemmed, stemmedTerm, currentStemmed, term.getValue());
+            } else {
+                stemmed.put(isUppercase?upperCase(stemmedTerm.toString()):stemmedTerm.toString(), term.getValue());
+            }
+            cache.put(isUppercase?upperCase(s):s, stemmedTerm.toString());
+
         }
         return stemmed;
     }
 
     private void sumFrequency(Map<String, String> stemmed, StringBuilder stemmedTerm, StringBuilder currentStemmed, String value) {
-        try{
+        try {
             int x = Integer.parseInt(currentStemmed.toString());
-            x+= Integer.parseInt(split(value,",")[0]);
+            x += Integer.parseInt(split(value, ",")[0]);
             currentStemmed.setLength(0);
-            currentStemmed.append(x).append(",0");
-            stemmed.replace(stemmedTerm.toString(),value,currentStemmed.toString());
-        }catch (Exception e){
+            currentStemmed.append(x).append(",1");
+            stemmed.replace(stemmedTerm.toString(), value, currentStemmed.toString());
+        } catch (Exception e) {
 
         }
     }
