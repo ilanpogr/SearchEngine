a92ab8309a04e43e7ae568c231d6143aa7ea2953
ilanpogr <ilanpogreb01@gmail.com>
2018-11-27 18:17:47 +0200

    branch init added CSS file, fxml ready, and View class

---
+++ src/main/java/View/IR_MenuView.java
@@ -0,0 +1,21 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+
+public class IR_MenuView {
+
+
+    public Button save_files_path;
+    public CheckBox stemmer_checkBox;
+    public ChoiceBox docs_language;
+    public Button reset_button;
+
+    public void BrowseDataSet(ActionEvent actionEvent) {
+    }
+
+    public void savePostingFile_DictionaryFile(ActionEvent actionEvent) {
+    }
+}
