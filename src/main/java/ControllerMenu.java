efa5e0135fce0b1d9531eb234a17ed22a4a27778
ilanpogr <ilanpogreb01@gmail.com>
2018-11-28 12:33:59 +0200

    GUI ready for merge. View and controller communicating. Properties File controlled by ControllerMenu.

---
+++ src/main/java/ControllerMenu.java
@@ -0,0 +1,93 @@
+import javafx.fxml.FXMLLoader;
+import view.IR_MenuView;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Observable;
+import java.util.Observer;
+
+public class ControllerMenu implements Observer {
+
+    private FXMLLoader fxmlLoader;
+    private Stage stage;
+    private Parent root;
+
+    private IR_MenuView ir_menuView;
+    private ModelMenu modelMenu;
+    private PropertiesFile propertiesFile;
+    private String[] propertyKeys = {"data.set.path", "save.files.path"};
+
+    private boolean isStemmer = false;
+
+    public boolean isStemmer() {
+        return isStemmer;
+    }
+
+
+    public ControllerMenu(String properties_file_name) {
+        stage = new Stage();
+        fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("menu/ir_menu.fxml"));
+        try {
+            root = fxmlLoader.load();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("menu/style_menu.css");
+        stage.setScene(scene);
+        modelMenu = new ModelMenu();
+        ir_menuView = fxmlLoader.getController();
+        ir_menuView.addObserver(this);
+        propertiesFile = new PropertiesFile(properties_file_name);
+    }
+
+    /**
+     * Opens the directory chooser
+     *
+     * @param operation : 1 -> Save; 0 Load
+     */
+    private void loadPathFromDirectoryChooser(int operation) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(stage);
+        if (selectedDirectory == null) {
+            return;
+        } else {
+            if (operation == 0) {
+                propertiesFile.putProperty(propertyKeys[0], selectedDirectory.getAbsolutePath());
+            } else {
+                propertiesFile.putProperty(propertyKeys[1], selectedDirectory.getAbsolutePath());
+            }
+        }
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o.equals(ir_menuView)) {
+            if (arg.equals("start")) {
+                if (ir_menuView.stemmer_checkBox.isSelected()){
+                    isStemmer = true;
+                    System.out.println("Stemmer option is: ON");
+                } else {
+                    System.out.println("Stemmer option is: OFF");
+                }
+            } else if (arg.equals("browse")) {
+                loadPathFromDirectoryChooser(0);
+                ir_menuView.start_bttn.setDisable(false);
+            } else if (arg.equals("save")) {
+                loadPathFromDirectoryChooser(1);
+            } else if (arg.equals("reset")) {
+                propertiesFile.resetProperties(propertyKeys);
+                ir_menuView.start_bttn.setDisable(true);
+            }
+        }
+    }
+
+    public void showStage() {
+        stage.show();
+    }
+}
---
+++ src/main/java/PropertiesFile.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesFile {
+
+    private Properties properties;
+
+    /**
+     * @param propertiesFileName
+     *          The name of the Properties file - relative to the
+     *          CLASSPATH - that you want to process using this class.
+     */
+    public PropertiesFile(String propertiesFileName) {
+
+        properties = new Properties();
+
+        // Load the properties file into the Properties object
+        try {
+            properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(propertiesFileName));
+            System.out.println("Properties file '" + propertiesFileName + "' loaded.");
+        } catch (IOException e) {
+            String message = "Exception while reading properties file '" + propertiesFileName + "':" + e.getLocalizedMessage();
+            System.out.println( );
+            throw new RuntimeException(message, e);
+        }
+    }
+
+
+
+    /**
+     * Fetches a single property whose value will be returned as a String.
+     * @param propertyName : The name of the property
+     * @return String - the value of the property
+     */
+    public String getProperty(String propertyName) {
+        String ret = "";
+        ret = properties.getProperty(propertyName);
+        System.out.println("Property value is '" + ret + "'");
+        return ret;
+    }
+
+    public void resetProperties(String[] propertiesArray){
+        for (String property : propertiesArray){
+            if (properties.getProperty(property) != null) {
+                properties.setProperty(property, "");
+            }
+        }
+    }
+
+    /**
+     * Adds a property to this Properties file.
+     * @param propertyName : property name
+     * @param propertyValue : property value
+     */
+    public void putProperty(String propertyName, String propertyValue) {
+        if (properties.getProperty(propertyName) == null) {
+            properties.put(propertyName, propertyValue);
+            System.out.println("new Property: " + propertyName + " , With value: " + propertyValue);
+        } else {
+            properties.setProperty(propertyName, propertyValue);
+            System.out.println("Property changed: " + propertyName + " , With value: " + propertyValue);
+        }
+    }
+}
